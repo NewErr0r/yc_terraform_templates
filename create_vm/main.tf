431f2aaa8c1962cf8ce5ae9a63f78b2cfc8de7ac
NewErr0r <nosenkodmitro@yandex.ru>
2022-11-17 17:09:48 +0300

    Add configuration main file

---
+++ create_vm/main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+}
+
+provider "yandex" {
+  token     = "<OAuth>"
+  cloud_id  = "<идентификатор облака>"
+  folder_id = "<идентификатор каталога>"
+  zone      = "<зона доступности по умолчанию>"
+}
